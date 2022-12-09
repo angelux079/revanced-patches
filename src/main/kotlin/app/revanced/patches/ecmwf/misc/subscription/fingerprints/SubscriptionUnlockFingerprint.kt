64b74f932ec97f54ab20f54a1121231e113b151f
Sven Koch <39488412+SVNKoch@users.noreply.github.com>
2022-12-09 15:03:01 +0100

    Remove Kachelmann related DMCA files

--- src/main/kotlin/app/revanced/patches/ecmwf/misc/subscription/fingerprints/SubscriptionUnlockFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.ecmwf.misc.subscription.fingerprints
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object SubscriptionUnlockFingerprint : MethodFingerprint(
-    "Z",
-    customFingerprint = { methodDef ->
-        methodDef.definingClass.endsWith("MapsActivity;") && methodDef.name == "t0"
-    }
-)
